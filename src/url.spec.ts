31baec2cdc6a4b3cb7c03dddd5bbe1a1fa3bd71a
童锴 <346762712@qq.com>
2021-12-17 15:45:51 +0800

    feat(sugar): add Url compose util

--- src/url.spec.ts
+++ src/url.spec.ts
@@ -1,21 +1,46 @@
-import { urlSplit } from './url';
+import { UrlSplit } from '.';
+import { Url } from './url';
 
 describe('url split', () => {
   it('split full url', () => {
-    const split = urlSplit('/root?name=sora&age=18');
+    const split = Url.split('/root?name=sora&age=18');
 
     expect(split).toEqual({ root: '/root', querys: { name: 'sora', age: '18' }});
   });
 
   it('split url without querys', () => {
-    const split = urlSplit('/root');
+    const split = Url.split('/root');
 
     expect(split).toEqual({ root: '/root', querys: {}});
   });
 
   it('split url with illegal query', () => {
-    const split = urlSplit('/root?name=sora&age12318');
+    const split = Url.split('/root?name=sora&age12318');
 
     expect(split).toEqual({ root: '/root', querys: { name: 'sora' }});
+  });
+
+  it('compose root', () => {
+    const split = { root: '/root' };
+
+    expect(Url.compose(split)).toBe('/root');
+  });
+
+  it('compose querys', () => {
+    const split = { querys: { name: 'sora' } };
+
+    expect(Url.compose(split)).toBe('name=sora');
+  });
+
+  it('compose root and querys', () => {
+    const split = { root: '/root', querys: { name: 'sora' } };
+
+    expect(Url.compose(split)).toBe('/root?name=sora');
+  });
+
+  it('compose width empty split', () => {
+    const split = {} as unknown as UrlSplit;
+
+    expect(Url.compose(split)).toBe('');
   });
 });
--- src/url.ts
+++ src/url.ts
@@ -1,5 +1,3 @@
-import { isUrl } from './validate';
-
 export interface UrlSplit {
   root: string;
   querys: {
@@ -7,27 +5,44 @@ export interface UrlSplit {
   };
 }
 
-export function urlSplit(url: string): UrlSplit {
-  const split: UrlSplit = {
-    root: '',
-    querys: {}
-  };
+export namespace Url {
+  /** split url's root path and querys */
+  export function split(url: string): UrlSplit {
+    const split: UrlSplit = {
+      root: '',
+      querys: {}
+    };
 
-  if (url.includes('?')) {
-    const match = url.match(/(.+?)\?(.+)/)!;
-    split.root = match[1];
-    const queryPath = match[2];
+    if (url.includes('?')) {
+      const match = url.match(/(.+?)\?(.+)/)!;
+      split.root = match[1];
+      const queryPath = match[2];
 
-    for (const query of queryPath.split('&')) {
-      const querySplit = query.split('=');
+      for (const query of queryPath.split('&')) {
+        const querySplit = query.split('=');
 
-      if (querySplit.length === 2) {
-        split.querys[querySplit[0]] = querySplit[1];
+        if (querySplit.length === 2) {
+          split.querys[querySplit[0]] = querySplit[1];
+        }
       }
+    } else {
+      split.root = url;
     }
-  } else {
-    split.root = url;
+
+    return split;
   }
 
-  return split;
+  /** compose root into url */
+  export function compose(split: Pick<UrlSplit, 'root'>): string;
+  /** compose querys into url */
+  export function compose(split: Pick<UrlSplit, 'querys'>): string;
+  /** compose root path and querys into url */
+  export function compose(split: UrlSplit): string;
+  /** compose root path and querys into url */
+  export function compose(split: Partial<UrlSplit>) {
+    const root = split.root ?? '';
+    const querys = split.querys ? Object.entries(split.querys).map(([key, value]) => `${key}=${value}`).join('') : '';
+
+    return `${root}${(!split.querys || !split.root) ? '' : '?'}${querys}`
+  }
 }
