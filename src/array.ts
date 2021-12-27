d55b24b83889ac739f29ac957f55cb5a744ab04a
童锴 <346762712@qq.com>
2021-12-27 09:54:16 +0800

    feat(sugar): add array swap util

--- src/array.ts
+++ src/array.ts
@@ -9,7 +9,7 @@ export namespace Array {
   /** remove the elements of given index array */
   export function remove<T>(array: T[], indexs: number[]): T[];
   /** remove elements in array */
-  export function remove<T>(arr: T[], arg1: ((value: T) => unknown) | number | number[], arg2 = 1): T[] {
+  export function remove<T>(arr: T[], arg1: PredicateCallback<T> | number | number[], arg2 = 1): T[] {
     if (typeof arg1 === 'function') {
       return arr.filter(item => !arg1(item));
     } 
@@ -27,5 +27,35 @@ export namespace Array {
     arr.splice(arg1, arg2);
 
     return arr;
+  }
+
+  function swapHelper<T>(array: T[], idx1: number, idx2: number): T[] {
+    if(idx1 < 0 || idx1 >= array.length || idx2 < 0 || idx2 >= array.length) return array;
+
+    const temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
+
+    return array;
+  }
+
+  /** swap two element in array by given index */
+  export function swap<T>(array: T[], idx1: number, idx2: number): T[];
+  /** swap two element that meet the condition specified in a callback function in array */
+  export function swap<T, P extends PredicateCallback<T>>(array: T[], pred1: P, pred2: P): T[];
+  /** swap two element in array */
+  export function swap<T, P extends number | PredicateCallback<T>>(array: T[], arg1: P, arg2: P): T[] {
+    if(typeof arg1 === 'number' && typeof arg2 === 'number') {
+      return swapHelper(array, arg1, arg2)
+    }
+
+    if(typeof arg1 === 'function' && typeof arg2 === 'function') {
+      const idx1 = array.findIndex(item => arg1(item));
+      const idx2 = array.findIndex(item => arg2(item));
+
+      return swapHelper(array, idx1, idx2)
+    }
+
+    return array;
   }
 }
