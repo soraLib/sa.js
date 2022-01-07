031f70980d5bed60a849b09a677a5e136b103890
童锴 <346762712@qq.com>
2022-01-07 11:38:26 +0800

    feat(sugar): deep partial type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
 export type PredicateCallback<T> = (value: T) => unknown;
 
 export type PickObject<T extends Object, D extends Object> = Pick<T, keyof D extends keyof T ? keyof D : keyof Partial<T>>;
+
+/** deep partial */
+export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]>; } : T;
