ad855e949439f916d8d713d66a72bc8cd5e75400
童锴 <346762712@qq.com>
2022-01-17 10:10:57 +0800

    feat: promiseAll predicate callback

--- src/promise.spec.ts
+++ src/promise.spec.ts
@@ -10,7 +10,7 @@ describe('Promise', () => {
   it('promise all test', async () => {
     const tasks = [
       () => 1,
-      () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 1000))
+      () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 100))
     ];
 
     const results = await promiseAll(tasks);
@@ -23,7 +23,7 @@ describe('Promise', () => {
 
     const results = await promiseAll([ 
       () => 1,
-      () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 1000)),
+      () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 100)),
       () => new Promise<string>((resolve, reject) => { setTimeout(() => {
         if(times < 2) {
           times += 1;
@@ -31,7 +31,7 @@ describe('Promise', () => {
         }
 
         resolve('3');
-      }, 1000) })
+      }, 100) })
     ], 3);
 
 
@@ -39,6 +39,46 @@ describe('Promise', () => {
   });
 
   it('promise all over retry times test', async () => {
+    let times = 0;
+
+    try {
+      await promiseAll([
+        () => 1,
+        () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 100)),
+        () => new Promise<number>((resolve, reject) => setTimeout(() => {
+          if(times < 2) {
+            times++;
+            reject(times);
+          }
+  
+          resolve(3);
+        }, 100))
+      ], 1);
+    } catch(err) {
+      expect(err).toEqual(times);
+    }
+  });
+
+  it('promise all with predicate test', async () => {
+    let times = 0;
+
+    const results = await promiseAll([ 
+      () => 1,
+      () => new Promise<number>((resolve) => setTimeout(() => resolve(2), 100)),
+      () => new Promise<string>((resolve, reject) => { setTimeout(() => {
+        if(times < 2) {
+          times += 1;
+          reject(times);
+        }
+
+        resolve('3');
+      }, 100) })
+    ], () => times < 3);
+
+    expect(results).toEqual([1, 2, '3']);
+  });
+
+  it('promise all with predicate error test', async () => {
     let times = 0;
 
     try {
@@ -52,8 +92,23 @@ describe('Promise', () => {
           }
   
           resolve(3);
-        }, 1000))
-      ], 1);
+        }, 100))
+      ], () => times < 1);
+    } catch(err) {
+      expect(err).toEqual(times);
+    }
+
+    try {
+      await promiseAll([
+        () => new Promise<number>((resolve, reject) => setTimeout(() => {
+          if(times < 2) {
+            times++;
+            reject(times);
+          }
+  
+          resolve(3);
+        }))
+      ]);
     } catch(err) {
       expect(err).toEqual(times);
     }
