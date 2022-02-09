b2155a262d6621442705158d5134c00cc9852fe4
童锴 <346762712@qq.com>
2022-02-09 16:28:08 +0800

    feat: add pick union type

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,16 @@ export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T
 
 /** Constructs a type by picking restricted object value */
 export type PickRestrictedObject<T extends object, R extends unknown> = Pick<T, {[P in keyof T]: T[P] extends R ? P : never }[keyof T]>;
+
+
+
+/**
+ * pick union type
+ * 
+ * @example
+ * 
+ * PickUnion<{ a: number, b: string }, 'a' | 'b'>
+ * 
+ * // => { a: number } | { b: string }
+ */
+export type PickUnion<T, K extends keyof T> = K extends K ? { [P in K]: T[P] } : never;
