518c610bd6f15ac86f613dd998e67068fb04a24d
童锴 <346762712@qq.com>
2022-04-02 18:06:56 +0800

    feat: multi and equal

--- src/equal.spec.ts
+++ src/equal.spec.ts
@@ -1,9 +1,16 @@
-import { multiOrEqual } from "./equal";
+import { multiAndEqual, multiOrEqual } from "./equal";
 
 describe('Equal', () => {
   it('multiple or equal test', () => {
     const num = 100;
     expect(multiOrEqual(100, num => num > 5, 0)).toBe(true);
     expect(multiOrEqual(100, num => num < 10, num)).toBe(true);
+  });
+
+  it('multiple and equal test', () => {
+    const num = 100;
+    expect(multiAndEqual(100, num => num > 5, 0)).toBe(false);
+    expect(multiAndEqual(100, num => num < 10, num)).toBe(false);
+    expect(multiAndEqual(100, num => num > 10, 100)).toBe(true);
   });
 });
--- src/equal.ts
+++ src/equal.ts
@@ -2,8 +2,12 @@ import { PredicateCallback } from "./types";
 
 type NotFunction<T> = T extends Function ? never : T;
 type MOET<T> = PredicateCallback<T> | T;
+
+const isPredicateCallback = <T>(value: unknown) : value is PredicateCallback<T> => typeof value === 'function';
+
+
 /**
- * return whether or not source is equal to target values or fit those predicates.
+ * return whether or not source is fit for any element of targets.
  * 
  * @example
  * 
@@ -13,7 +17,20 @@ type MOET<T> = PredicateCallback<T> | T;
  */
 export function multiOrEqual<T>(source: NotFunction<T>, ...targets: MOET<T>[]): boolean;
 export function multiOrEqual<T>(source: NotFunction<T>, ...targets: MOET<T>[]): boolean {
-  const isPredicateCallback = (value: unknown) : value is PredicateCallback<T> => typeof value === 'function';
-
-  return targets.some(v => isPredicateCallback(v) ? v(source) : source === v);
+  return targets.some(v => isPredicateCallback<T>(v) ? v(source) : source === v);
 }
+
+
+/**
+ * return whether or not source is fit for every element of targets.
+ * 
+ * @example
+ * 
+ * multiAndEqual(10, num => num > 5, 0);
+ * 
+ * // => false
+ */
+ export function multiAndEqual<T>(source: NotFunction<T>, ...targets: MOET<T>[]): boolean;
+ export function multiAndEqual<T>(source: NotFunction<T>, ...targets: MOET<T>[]): boolean {
+   return targets.every(v => isPredicateCallback<T>(v) ? v(source) : source === v);
+ }
