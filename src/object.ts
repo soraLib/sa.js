5959cb2d39c479e1f7a4187a37b68b237a93a552
童锴 <346762712@qq.com>
2022-04-22 09:38:47 +0800

    feat: object has

--- src/object.ts
+++ src/object.ts
@@ -14,3 +14,16 @@ export function setObjectValues<T extends object>(target: T, values: Partial<T>)
 
   return target;
 }
+
+/**
+ * determines whether target object contain the sub object.
+ * 
+ * @example
+ * 
+ * hasSubset({ a: 1, b: 2 }, { a: 1 })
+ * 
+ * // => true
+ */
+export function hasSubset<T extends object>(target: T, sub: object): boolean {
+  return Object.entries(sub).every(([key, value]) => target[key as keyof T] === value);
+}
